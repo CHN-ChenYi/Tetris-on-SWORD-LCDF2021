c0ad98a8fd690daa032f3a32baced806edc663bd
TO/GA <chenyi2000@zju.edu.cn>
2021-01-11 15:31:56 +0800

    finish ZigZagGen readme

--- ZigZagGen/ZigZagGen.v
+++ ZigZagGen/ZigZagGen.v
@@ -1,23 +1,5 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company:
-// Engineer:
-//
-// Create Date:    19:19:35 01/10/2021
-// Design Name:
-// Module Name:    ZigZagGen
-// Project Name:
-// Target Devices:
-// Tool versions:
-// Description:
-//
-// Dependencies:
-//
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-//
-//////////////////////////////////////////////////////////////////////////////////
+
 module ZigZagGen(
          input wire clk,
          input wire zigzag,
