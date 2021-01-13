3fc39262321bf7cb24a82db7b14e4b7ddbf34c73
QSCTech-Sange <3160105521@zju.edu.cn>
2021-01-13 13:39:08 +0800

    bottom collide

--- CollisionChecker.v
+++ CollisionChecker.v
@@ -14,9 +14,12 @@ module CollisionChecker(
 // 4  5  6  7
 // 0  1  2  3
 
-reg tempCollision;
+reg patternCollision;
+reg bottomCollision;
+// reg leftCollision;
+// reg rightCollision;
 
-// 十五个点的真实位置
+// realPosition for 16 points
 wire [8:0] realPos[0:15];
 assign realPos[0] = ((pos_y - 2'b11) * 4'b1010) + pos_x - 2'b11; 
 assign realPos[1] = ((pos_y - 2'b11) * 4'b1010) + pos_x - 2'b10; 
@@ -34,11 +37,41 @@ assign realPos[12] = (pos_y * 4'b1010) + pos_x - 2'b11;
 assign realPos[13] = (pos_y * 4'b1010) + pos_x - 2'b10; 
 assign realPos[14] = (pos_y * 4'b1010) + pos_x - 1'b1; 
 assign realPos[15] = (pos_y * 4'b1010) + pos_x; 
-assign collision = tempCollision;
 
+// from bottom to up, whether each row has at least a block
+wire [0:2] row;
+assign row[0] = |float[0:3];
+assign row[1] = |float[4:7];
+assign row[2] = |float[8:11];
+
+// once collide, collision = 1
+// assign collision = patternCollision | bottomCollision | leftCollision | rightCollision;
+assign collision = bottomCollision | patternCollision;
+
+// judge pattern
+//TODO: more conditions
 always @(posedge clk) begin
-  tempCollision <= (float[0] & static[realPos[0]]) | (float[1] & static[realPos[1]]) | (float[2] & static[realPos[2]]) | (float[3] & static[realPos[3]]) | (float[4] & static[realPos[4]]) | (float[5] & static[realPos[5]]) | (float[6] & static[realPos[6]]) | (float[7] & static[realPos[7]]) | (float[8] & static[realPos[8]]) | (float[9] & static[realPos[9]]) | (float[10] & static[realPos[10]]) | (float[11] & static[realPos[11]]) | (float[12] & static[realPos[12]]) | (float[13] & static[realPos[13]]) | (float[14] & static[realPos[14]]) | (float[15] & static[realPos[15]]); 
+  if (pos_x >= 4'b0011 && pos_x <= 4'b1010 && pos_y >= 5'b00011 && pos_y <= 5'b10100)
+    patternCollision <= (float[0] & static[realPos[0]]) | (float[1] & static[realPos[1]]) | (float[2] & static[realPos[2]]) | (float[3] & static[realPos[3]]) | (float[4] & static[realPos[4]]) | (float[5] & static[realPos[5]]) | (float[6] & static[realPos[6]]) | (float[7] & static[realPos[7]]) | (float[8] & static[realPos[8]]) | (float[9] & static[realPos[9]]) | (float[10] & static[realPos[10]]) | (float[11] & static[realPos[11]]) | (float[12] & static[realPos[12]]) | (float[13] & static[realPos[13]]) | (float[14] & static[realPos[14]]) | (float[15] & static[realPos[15]]); 
+  else
+    patternCollision <= 0;
+//   else if ()
 end
 
+
+// judge bottom
+// if pos_y < 0, then pos_y = 11111
+always @(posedge clk) begin
+  if (pos_y == 5'b11111)
+    bottomCollision <= 1;
+  else if (row[0] == 1'b1)
+    bottomCollision <= ~ (pos_y[1:0] == 2'b11 || |pos_y[4:2]);
+  else if (row[1] == 1'b1)
+    bottomCollision <= ~ (|pos_y[4:1]);
+  else if (row[2] == 1'b1)
+    bottomCollision <= ~ (|pos_y);
+  else
+	 bottomCollision <= 0;
+end
 
 endmodule
